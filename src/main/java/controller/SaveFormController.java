8f6321adb72e7705111a68e29d5a05e55e6bd8e6
Pubudu Janith <pubudujanith123@gmail.com>
2022-08-10 17:06:43 +0530

    Implement new,open,save and print logics

---
+++ src/main/java/controller/SaveFormController.java
@@ -0,0 +1,82 @@
+package controller;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.DirectoryChooser;
+import java.io.*;
+import java.util.Optional;
+
+public class SaveFormController {
+
+    public AnchorPane pneSaveForm;
+    public JFXTextField txtFileName;
+    public JFXButton btnOK;
+    public JFXButton btnBrowsePath;
+    public JFXTextField txtFilePath;
+    private String content;
+    private File file;
+
+    public void setData(String editorContent) {
+        content = editorContent;
+    }
+
+    public void btnOK_OnAction(ActionEvent actionEvent) throws IOException {
+        if (txtFileName.getText().isBlank()) {
+            new Alert(Alert.AlertType.ERROR,"File name cannot be empty!").showAndWait();
+            txtFileName.selectAll();
+            txtFileName.requestFocus();
+            txtFilePath.clear();
+            return;
+        }
+        if (!isName(txtFileName.getText())) {
+            new Alert(Alert.AlertType.ERROR,"Invalid file name format!").showAndWait();
+            txtFileName.selectAll();
+            txtFileName.requestFocus();
+            txtFilePath.clear();
+            return;
+        }
+        if (txtFilePath.getText().isEmpty()) {
+            new Alert(Alert.AlertType.ERROR,"Save location path is empty").showAndWait();
+            btnBrowsePath.requestFocus();
+            return;
+        }
+        pneSaveForm.getScene().getWindow().hide();
+        file.createNewFile();
+        byte[] bytes = content.getBytes();
+        FileOutputStream fos = new FileOutputStream(file);
+        for (byte aByte : bytes) {
+            fos.write(aByte + 1);
+        }
+        new Alert(Alert.AlertType.INFORMATION,"Files has been saved successfully").showAndWait();
+    }
+    public void btnBrowsePath_OnAction(ActionEvent actionEvent) {
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        directoryChooser.setInitialDirectory(new File(System.getProperty("user.home")));
+        directoryChooser.setTitle("Select a directory to save");
+        File directory = directoryChooser.showDialog(pneSaveForm.getScene().getWindow());
+        if (directory != null) {
+            file = new File(directory.getAbsolutePath(), txtFileName.getText() + ".dep9");
+            if (file.exists()) {
+                Optional<ButtonType> result = new Alert(Alert.AlertType.INFORMATION,
+                        file.getName()+ " file is already exists. Do you want to overwrite?",
+                        ButtonType.YES, ButtonType.NO).showAndWait();
+                if (result.get() == ButtonType.NO) {
+                    return;
+                }
+            }
+            txtFilePath.setText(file.getAbsolutePath());
+        }
+    }
+
+    private boolean isName(String input) {
+        char[] chars = input.toCharArray();
+        for (char aChar : chars) {
+            if (!Character.isLetter(aChar) && aChar != ' ') return false;
+        }
+        return true;
+    }
+}
--- src/main/java/controller/TextEditorFormController.java
+++ src/main/java/controller/TextEditorFormController.java
@@ -3,18 +3,22 @@ package controller;
 import com.jfoenix.controls.JFXTextArea;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.print.Printer;
+import javafx.print.PrinterJob;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.MenuItem;
-import javafx.scene.web.HTMLEditor;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.FileChooser;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
-
-import java.io.IOException;
+import java.io.*;
+import java.net.URL;
 
 public class TextEditorFormController {
+    public AnchorPane pneContainer;
     public JFXTextArea txtEditor;
     public MenuItem mnuNew;
     public MenuItem mnuOpen;
@@ -28,34 +32,100 @@ public class TextEditorFormController {
     public MenuItem mnuAbout;
 
     public void initialize() {
-        mnuNew.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                txtEditor.setText("");
-            }
-        });
-        mnuClose.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                Platform.exit();
-            }
-        });
-        mnuAbout.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                try {
-                    Parent root = FXMLLoader.load(this.getClass().getResource("/view/AboutForm.fxml"));
-                    Scene scene = new Scene(root);
-                    Stage stage = new Stage();
-                    stage.initModality(Modality.APPLICATION_MODAL);
-                    stage.setTitle("About");
-                    stage.setScene(scene);
-                    stage.centerOnScreen();
-                    stage.show();
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
+    }
+
+    public void mnuNew_OnAction(ActionEvent actionEvent) {
+        txtEditor.setText("");
+    }
+
+    public void mnuOpen_OnAction(ActionEvent actionEvent) throws IOException {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setInitialDirectory(new File(System.getProperty("user.home")));
+        fileChooser.setTitle("Select a file to open");
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("*.txt","*.txt"));
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("*.dep9","*.dep9"));
+        File file = fileChooser.showOpenDialog(pneContainer.getScene().getWindow());
+        if (file != null) {
+            FileInputStream fis = new FileInputStream(file);
+            BufferedInputStream bis = new BufferedInputStream(fis);
+            if (file.getName().contains(".txt")) {
+                while (true) {
+                    byte[] buffer = new byte[1024 * 10];
+                    int read = bis.read(buffer);
+                    if (read == -1) break;
+                    for (byte b : buffer) {
+                        txtEditor.appendText(String.valueOf((char) b));
+                    }
                 }
+                bis.close();
             }
-        });
+            else if (file.getName().contains(".dep9")) {
+                while (true) {
+                    byte[] buffer = new byte[1024 * 10];
+                    int read = bis.read(buffer);
+                    if (read == -1) break;
+                    for (int i = 0; i < read; i++) {
+                        buffer[i] = (byte) (buffer[i] - 1);
+                        txtEditor.appendText(String.valueOf((char)buffer[i]));
+                    }
+                }
+                bis.close();
+            }
+        }
+    }
+
+    public void mnuSave_OnAction(ActionEvent actionEvent) throws IOException {
+        URL resource = this.getClass().getResource("/view/SaveForm.fxml");
+        FXMLLoader fxmlLoader = new FXMLLoader(resource);
+        Parent root = fxmlLoader.load();
+
+        SaveFormController ctrl = fxmlLoader.getController();
+        ctrl.setData(txtEditor.getText());
+
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.setScene(scene);
+        stage.setTitle("Save File");
+        stage.setResizable(false);
+        stage.sizeToScene();
+        stage.centerOnScreen();
+        stage.show();
+    }
+
+    public void mnuPrint_OnAction(ActionEvent actionEvent) {
+        if (Printer.getDefaultPrinter() == null) {
+            new Alert(Alert.AlertType.ERROR,"No default printer has been selected").showAndWait();
+            return;
+        }
+        PrinterJob printerJob = PrinterJob.createPrinterJob();
+        if (printerJob != null) {
+            printerJob.showPageSetupDialog(pneContainer.getScene().getWindow());
+            boolean success = printerJob.printPage(txtEditor);
+            if (success) {
+                printerJob.endJob();
+            }
+            else {
+                new Alert(Alert.AlertType.ERROR,"Failed to print, try again").showAndWait();
+            }
+        }
+        else {
+            new Alert(Alert.AlertType.ERROR,"Failed to initialize a new printer job").show();
+        }
+    }
+
+    public void mnuClose_OnAction(ActionEvent actionEvent) {
+        Platform.exit();
+    }
+
+    public void mnuAbout_OnAction(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(this.getClass().getResource("/view/AboutForm.fxml"));
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.setTitle("About");
+        stage.setScene(scene);
+        stage.centerOnScreen();
+        stage.show();
     }
 }
